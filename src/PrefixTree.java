60af2bc56fac4f7000adcc4d1291fdbbd63a1b29
Daniehao <hao.yij@husky.neu.edu>
2020-11-03 17:02:41 -0500

    update codeOperations

--- src/PrefixTree.java
+++ src/PrefixTree.java
@@ -1,2 +1,17 @@
+/**
+ * Class to implement operations on prefix tree, for example, look up the character by given the
+ * prefix code or insert a prefix code to the tree's root.
+ */
 public class PrefixTree {
+
+  public Node insert(Character c, String code, int symbolNum) {
+    Node root = new trieElementNode(symbolNum);
+    for (int i = 0; i < code.length(); i++) {
+      root.add(code.charAt(i));
+    }
+  }
+
+  public Character lookUp() {
+
+  }
 }
